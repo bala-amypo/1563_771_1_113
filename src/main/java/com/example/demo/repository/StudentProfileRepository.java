58c39f2e29d949370b38504ea99ee1fac365bc07
bala-amypo <bala@amypo.in>
2025-12-16 16:03:09 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/StudentProfileRepository.java
@@ -0,0 +1,13 @@
+package com.example.Academy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.Academy.entity.StudentProfile;
+
+@Repository
+public interface StudentProfileRepository extends JpaRepository<StudentProfile,Integer> {
+
+    StudentProfile findById(Long id);
+    
+}
---
+++ src/main/java/com/example/demo/service/impl/StudentProfileServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.Academy.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.Academy.entity.StudentProfile;
+import com.example.Academy.repository.StudentProfileRepository;
+import com.example.Academy.service.StudentProfileService;
+
+@Service
+public class StudentProfileServiceImpl implements StudentProfileService {
+
+    @Autowired
+     StudentProfileRepository studentRepository;
+
+    @Override
+    public StudentProfile createStudent(StudentProfile dto) {
+        return studentRepository.save(dto);
+    }
+
+    @Override
+    public StudentProfile getStudentById(Long id) {
+        return studentRepository.findById(id);
+    }
+
+    @Override
+    public List<StudentProfile> getAllStudent() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public StudentProfile updateRepeatOffenderStatus(Long studentId) {
+        StudentProfile student = getStudentById(studentId);
+        student.setIsRepeatOffender(true);
+        return studentRepository.save(student);
+    }
+}
