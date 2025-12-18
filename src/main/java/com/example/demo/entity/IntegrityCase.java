8c1290c26c24d4a5af687e5f5187aaa9b2a23106
bala-amypo <bala@amypo.in>
2025-12-18 15:57:43 +0000

    User commit

--- src/main/java/com/example/demo/entity/IntegrityCase.java
+++ src/main/java/com/example/demo/entity/IntegrityCase.java
@@ -1,78 +1,78 @@
-// package com.example.Academy.entity;
+package com.example.Academy.entity;
 
-// import java.time.LocalDate;
-// import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-// public class IntegrityCase {
-//     private Long id;
-//     private StudentProfile studentProfile;
-//     private String courseCode;
-//     private String instructorName;
-//     private String description;
-//     private String status;
-//     private LocalDate incidentDate;
-//     private LocalDateTime createAt;
-//     public IntegrityCase(){
+public class IntegrityCase {
+    private Long id;
+    private StudentProfile studentProfile;
+    private String courseCode;
+    private String instructorName;
+    private String description;
+    private String status;
+    private LocalDate incidentDate;
+    private LocalDateTime createAt;
+    public IntegrityCase(){
 
-//     }
-//     public IntegrityCase(StudentProfile studentProfile, String courseCode, String instructorName, String description,
-//             String status, LocalDate incidentDate, LocalDateTime createAt) {
-//         this.studentProfile = studentProfile;
-//         this.courseCode = courseCode;
-//         this.instructorName = instructorName;
-//         this.description = description;
-//         this.status = status;
-//         this.incidentDate = incidentDate;
-//         this.createAt = createAt;
-//     }
-//     public Long getId() {
-//         return id;
-//     }
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
-//     public StudentProfile getStudentProfile() {
-//         return studentProfile;
-//     }
-//     public void setStudentProfile(StudentProfile studentProfile) {
-//         this.studentProfile = studentProfile;
-//     }
-//     public String getCourseCode() {
-//         return courseCode;
-//     }
-//     public void setCourseCode(String courseCode) {
-//         this.courseCode = courseCode;
-//     }
-//     public String getInstructorName() {
-//         return instructorName;
-//     }
-//     public void setInstructorName(String instructorName) {
-//         this.instructorName = instructorName;
-//     }
-//     public String getDescription() {
-//         return description;
-//     }
-//     public void setDescription(String description) {
-//         this.description = description;
-//     }
-//     public String getStatus() {
-//         return status;
-//     }
-//     public void setStatus(String status) {
-//         this.status = status;
-//     }
-//     public LocalDate getIncidentDate() {
-//         return incidentDate;
-//     }
-//     public void setIncidentDate(LocalDate incidentDate) {
-//         this.incidentDate = incidentDate;
-//     }
-//     public LocalDateTime getCreateAt() {
-//         return createAt;
-//     }
-//     public void setCreateAt(LocalDateTime createAt) {
-//         this.createAt = createAt;
-//     }
+    }
+    public IntegrityCase(StudentProfile studentProfile, String courseCode, String instructorName, String description,
+            String status, LocalDate incidentDate, LocalDateTime createAt) {
+        this.studentProfile = studentProfile;
+        this.courseCode = courseCode;
+        this.instructorName = instructorName;
+        this.description = description;
+        this.status = status;
+        this.incidentDate = incidentDate;
+        this.createAt = createAt;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public StudentProfile getStudentProfile() {
+        return studentProfile;
+    }
+    public void setStudentProfile(StudentProfile studentProfile) {
+        this.studentProfile = studentProfile;
+    }
+    public String getCourseCode() {
+        return courseCode;
+    }
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+    public String getInstructorName() {
+        return instructorName;
+    }
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public LocalDate getIncidentDate() {
+        return incidentDate;
+    }
+    public void setIncidentDate(LocalDate incidentDate) {
+        this.incidentDate = incidentDate;
+    }
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
     
 
-// }
+}
