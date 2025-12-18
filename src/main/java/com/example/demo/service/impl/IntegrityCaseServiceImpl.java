54a13751052c86728ab4068699f7b191a6bbfc25
bala-amypo <bala@amypo.in>
2025-12-18 16:30:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/IntegrityCaseServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.Academy.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.Academy.entity.IntegrityCase;
+import com.example.Academy.repository.IntegrityCaseRepository;
+import com.example.Academy.service.IntegrityCaseService;
+
+@Service
+public class IntegrityCaseServiceImpl implements IntegrityCaseService {
+
+    @Autowired
+     IntegrityCaseRepository caseRepository;
+
+    @Override
+    public IntegrityCase createCase(IntegrityCase integrityCase) {
+        return caseRepository.save(integrityCase);
+    }
+
+    @Override
+    public IntegrityCase updateCaseStatus(Long caseId, String status) {
+        IntegrityCase integrityCase = getCaseById(caseId);
+        integrityCase.setStatus(status);
+        return caseRepository.save(integrityCase);
+    }
+
+    @Override
+public List<IntegrityCase> getCasesByStudent(Long studentId) {
+    
+    return caseRepository.findByStudentId(studentId);
+}
+    @Override
+    public List<IntegrityCase> getAllCases() {
+        return caseRepository.findAll();
+    }
+
+    @Override
+    public IntegrityCase getCaseById(Long id) {
+        Optional<IntegrityCase> lis =caseRepository.findById(id);
+        return lis.orElse(null);
+    }
+}
