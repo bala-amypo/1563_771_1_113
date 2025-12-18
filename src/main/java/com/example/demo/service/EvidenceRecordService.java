e4a8c5755c5bb67c1b27d15d531ad7ec5631672b
bala-amypo <bala@amypo.in>
2025-12-18 15:27:41 +0000

    User commit

--- src/main/java/com/example/demo/service/EvidenceRecordService.java
+++ src/main/java/com/example/demo/service/EvidenceRecordService.java
@@ -1,14 +1,14 @@
-package com.example.Academy.service;
+// package com.example.Academy.service;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.stereotype.Service;
+// import org.springframework.stereotype.Service;
 
-import com.example.Academy.entity.EvidenceRecord;
-@Service
-public interface EvidenceRecordService {
-    EvidenceRecord submitEvidence(EvidenceRecord evidence);
-    List<EvidenceRecord> getEvidenceByCase(Long caseId);
-    EvidenceRecord getEvidenceById(Long id);
-    EvidenceRecord getAllEvidence();
-}
+// import com.example.Academy.entity.EvidenceRecord;
+// @Service
+// public interface EvidenceRecordService {
+//     EvidenceRecord submitEvidence(EvidenceRecord evidence);
+//     List<EvidenceRecord> getEvidenceByCase(Long caseId);
+//     EvidenceRecord getEvidenceById(Long id);
+//     EvidenceRecord getAllEvidence();
+// }
--- src/main/java/com/example/demo/service/RepeatOffenderRecordService.java
+++ src/main/java/com/example/demo/service/RepeatOffenderRecordService.java
@@ -1,13 +1,13 @@
-package com.example.Academy.service;
+// package com.example.Academy.service;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.stereotype.Service;
+// import org.springframework.stereotype.Service;
 
-import com.example.Academy.entity.RepeatOffenderRecord;
-@Service
-public interface RepeatOffenderRecordService {
-    RepeatOffenderRecord refreshRepeatOffernderData(Long studentId);
-    RepeatOffenderRecord getRecordByStudent( Long studentId);
-    List<PenaltyActionService> getAllRepeatOffenders();
-}
+// import com.example.Academy.entity.RepeatOffenderRecord;
+// @Service
+// public interface RepeatOffenderRecordService {
+//     RepeatOffenderRecord refreshRepeatOffernderData(Long studentId);
+//     RepeatOffenderRecord getRecordByStudent( Long studentId);
+//     List<PenaltyActionService> getAllRepeatOffenders();
+// }
