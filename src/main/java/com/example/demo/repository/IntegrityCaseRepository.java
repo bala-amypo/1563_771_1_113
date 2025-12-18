22e97ef5b9212fd7b711efeeaa67f8065fe5e15d
bala-amypo <bala@amypo.in>
2025-12-18 16:19:09 +0000

    User commit

--- src/main/java/com/example/demo/repository/IntegrityCaseRepository.java
+++ src/main/java/com/example/demo/repository/IntegrityCaseRepository.java
@@ -1,9 +1,9 @@
-package com.example.Academy.repository;
+package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.Academy.entity.IntegrityCase;
+import com.example.demo.entity.IntegrityCase;
 
 
 @Repository
