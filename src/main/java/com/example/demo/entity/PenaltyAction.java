36d81f6bbb386456299dba01ba9153996056c501
bala-amypo <bala@amypo.in>
2025-12-19 08:51:37 +0000

    User commit

--- src/main/java/com/example/demo/entity/PenaltyAction.java
+++ src/main/java/com/example/demo/entity/PenaltyAction.java
@@ -9,8 +9,8 @@ public class PenaltyAction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "integrity_case_id", nullable = false)
+    // @ManyToOne
+    // @JoinColumn(name = "integrity_case_id", nullable = false)
     private IntegrityCase integrityCase;
 
     private String penaltyType;
@@ -18,7 +18,54 @@ public class PenaltyAction {
     private String issuedBy;
     private LocalDateTime issuedAt;
 
-    public PenaltyAction() {}
+    public PenaltyAction(){
 
-    // Getters and Setters
+    }
+    public PenaltyAction(IntegrityCase integrityCase, String penaltyType, String details, String issuedBy,
+            LocalDateTime issuedAt) {
+        this.integrityCase = integrityCase;
+        this.penaltyType = penaltyType;
+        this.details = details;
+        this.issuedBy = issuedBy;
+        this.issuedAt = issuedAt;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public IntegrityCase getIntegrityCase() {
+        return integrityCase;
+    }
+    public void setIntegrityCase(IntegrityCase integrityCase) {
+        this.integrityCase = integrityCase;
+    }
+    public String getPenaltyType() {
+        return penaltyType;
+    }
+    public void setPenaltyType(String penaltyType) {
+        this.penaltyType = penaltyType;
+    }
+    public String getDetails() {
+        return details;
+    }
+    public void setDetails(String details) {
+        this.details = details;
+    }
+    public String getIssuedBy() {
+        return issuedBy;
+    }
+    public void setIssuedBy(String issuedBy) {
+        this.issuedBy = issuedBy;
+    }
+    public LocalDateTime getIssuedAt() {
+        return issuedAt;
+    }
+    public void setIssuedAt(LocalDateTime issuedAt) {
+        this.issuedAt = issuedAt;
+    }
+    
+    
 }
+
--- src/main/java/com/example/demo/entity/StudentProfile.java
+++ src/main/java/com/example/demo/entity/StudentProfile.java
@@ -1,114 +1,102 @@
 package com.example.demo.entity;
+import java.util.*;
 import com.example.demo.entity.IntegrityCase;
-
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
+import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 @Entity
 public class StudentProfile {
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long id;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; 
+@Column(unique = true)
+private String studentid;
+private String name;
+@Column(unique = true)
+private String email;
+private String program;
+private Integer yearLevel;
 
-    @Column(unique = true, nullable = false)
-    private String studentId; 
-
-    private String name; 
-
-    @Column(unique = true, nullable = false)
-    private String email;
-
-    private String program; 
-    
-    private Integer yearLevel; 
-
-    private Boolean isRepeatOffender = false; 
-
-    private LocalDateTime createdAt; 
-
-   
-@OneToMany(mappedBy = "studentProfile", cascade = CascadeType.ALL)
-private List<IntegrityCase> integrityCases = new ArrayList<>();
-    @PrePersist
-    protected void onCreate() {
-        this.createdAt = LocalDateTime.now(); //
-    }
-
-    public StudentProfile() {} //
-
-    public StudentProfile(String studentId, String name, String email, String program, Integer yearLevel) {
-        this.studentId = studentId;
-        this.name = name;
-        this.email = email;
-        this.program = program;
-        this.yearLevel = yearLevel;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(String studentId) {
-        this.studentId = studentId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getProgram() {
-        return program;
-    }
-
-    public void setProgram(String program) {
-        this.program = program;
-    }
-
-    public Integer getYearLevel() {
-        return yearLevel;
-    }
-
-    public void setYearLevel(Integer yearLevel) {
-        this.yearLevel = yearLevel;
-    }
-
-    public Boolean getIsRepeatOffender() {
-        return isRepeatOffender;
-    }
-
-    public void setIsRepeatOffender(Boolean isRepeatOffender) {
-        this.isRepeatOffender = isRepeatOffender;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
+private Boolean isRepeatOffender = false;
+private LocalDateTime createdAt;
+@PrePersist
+protected void onCreate() {
+this.createdAt = LocalDateTime.now();
 }
+@OneToMany(mappedBy="studentProfile")
+public List<IntegrityCase> lis=new ArrayList<>();
+public StudentProfile(String studentid, String name, String email, String program, Integer yearLevel,
+Boolean isRepeatOffender, LocalDateTime createdAt) {
+this.studentid = studentid;
+this.name = name;
+this.email = email;
+this.program = program;
+this.yearLevel = yearLevel;
+this.isRepeatOffender = isRepeatOffender;
+this.createdAt = createdAt;
+}
+
+
+public StudentProfile(){
+
+}
+
+public Long getId() {
+return id;
+}
+public void setId(Long id) {
+this.id = id;
+}
+public String getStudentid() {
+return studentid;
+}
+public void setStudentid(String studentid) {
+this.studentid = studentid;
+}
+public String getName() {
+return name;
+}
+public void setName(String name) {
+this.name = name;
+}
+public String getEmail() {
+return email;
+}
+public void setEmail(String email) {
+this.email = email;
+}
+public String getProgram() {
+return program;
+}
+public void setProgram(String program) {
+this.program = program;
+}
+public Integer getYearLevel() {
+return yearLevel;
+}
+public void setYearLevel(Integer yearLevel) {
+this.yearLevel = yearLevel;
+}
+public Boolean getIsRepeatOffender() {
+return isRepeatOffender;
+}
+public void setIsRepeatOffender(Boolean isRepeatOffender) {
+this.isRepeatOffender = isRepeatOffender;
+}
+public LocalDateTime getCreatedAt() {
+return createdAt;
+}
+public void setCreatedAt(LocalDateTime createdAt) {
+this.createdAt = createdAt;
+}
+
+
+}
+
+
+
+
+
