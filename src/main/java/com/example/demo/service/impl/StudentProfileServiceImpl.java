ff14ec829baaf8edefd522e444b2cb72ca6b3398
bala-amypo <bala@amypo.in>
2025-12-19 10:28:37 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentProfileServiceImpl.java
@@ -1,42 +1,39 @@
-package com.example.demo.service.impl;
+package com.example.Academy.service.impl;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.entity.StudentProfile;
-import com.example.demo.repository.StudentProfileRepository;
-import com.example.demo.service.StudentProfileService;
+import com.example.Academy.entity.StudentProfile;
+import com.example.Academy.repository.StudentProfileRepository;
+import com.example.Academy.service.StudentProfileService;
+
 @Service
 public class StudentProfileServiceImpl implements StudentProfileService {
-@Autowired
-private StudentProfileRepository studentRepository;
-@Override
-public StudentProfile createStudent(StudentProfile dto) {
-return studentRepository.save(dto);
+
+    @Autowired
+     StudentProfileRepository studentRepository;
+
+    @Override
+    public StudentProfile createStudent(StudentProfile dto) {
+        return studentRepository.save(dto);
+    }
+
+    @Override
+    public StudentProfile getStudentById(Long id) {
+        return studentRepository.findById(id);
+    }
+
+    @Override
+    public List<StudentProfile> getAllStudent() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public StudentProfile updateRepeatOffenderStatus(Long studentId) {
+        StudentProfile student = getStudentById(studentId);
+        student.setIsRepeatOffender(true);
+        return studentRepository.save(student);
+    }
 }
-@Override
-public StudentProfile getStudentById(Long id) {
-return studentRepository.findById(id)
-.orElseThrow(() -> new RuntimeException("Student not found"));
-}
-
-@Override
-public List<StudentProfile> getAllStudents() {
-return studentRepository.findAll();
-}
-
-@Override
-public StudentProfile updateRepeatOffenderStatus(Long studentId) {
-StudentProfile student = getStudentById(studentId);
-student.setIsRepeatOffender(true);
-return studentRepository.save(student);
-}
-}
-
-
-
-
-
-
