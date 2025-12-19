4945e1576cb36555c8cf50fade51467d572ee13b
bala-amypo <bala@amypo.in>
2025-12-19 04:56:16 +0000

    User commit

--- src/main/java/com/example/demo/controller/IntegrityCaseController.java
+++ src/main/java/com/example/demo/controller/IntegrityCaseController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
-import com.example.demo.entity.StudentProfile;
 
 import com.example.demo.entity.IntegrityCase;
 import com.example.demo.service.IntegrityCaseService;
