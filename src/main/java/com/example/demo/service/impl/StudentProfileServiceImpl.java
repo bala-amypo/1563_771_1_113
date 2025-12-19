f11c8fc65f5e349ed54d6e9d230748acfc4d64db
bala-amypo <bala@amypo.in>
2025-12-19 04:41:18 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentProfileServiceImpl.java
@@ -1,39 +1,39 @@
-package com.example.demo.service.impl;
+// package com.example.demo.service.impl;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-import com.example.demo.entity.StudentProfile;
-import com.example.demo.repository.StudentProfileRepository;
-import com.example.demo.service.StudentProfileService;
-@Service
-public class StudentProfileServiceImpl implements StudentProfileService {
-@Autowired
-private StudentProfileRepository studentRepository;
-@Override
-public StudentProfile createStudent(StudentProfile dto) {
-return studentRepository.save(dto);
-}
-@Override
-public StudentProfile getStudentById(Long id) {
-return studentRepository.findById(id)
-.orElseThrow(() -> new RuntimeException("Student not found"));
-}
+// import com.example.demo.entity.StudentProfile;
+// import com.example.demo.repository.StudentProfileRepository;
+// import com.example.demo.service.StudentProfileService;
+// @Service
+// public class StudentProfileServiceImpl implements StudentProfileService {
+// @Autowired
+// private StudentProfileRepository studentRepository;
+// @Override
+// public StudentProfile createStudent(StudentProfile dto) {
+// return studentRepository.save(dto);
+// }
+// @Override
+// public StudentProfile getStudentById(Long id) {
+// return studentRepository.findById(id)
+// .orElseThrow(() -> new RuntimeException("Student not found"));
+// }
 
-@Override
-public List<StudentProfile> getAllStudents() {
-return studentRepository.findAll();
-}
+// @Override
+// public List<StudentProfile> getAllStudents() {
+// return studentRepository.findAll();
+// }
 
-@Override
-public StudentProfile updateRepeatOffenderStatus(Long studentId) {
-StudentProfile student = getStudentById(studentId);
-student.setIsRepeatOffender(true);
-return studentRepository.save(student);
-}
-}
+// @Override
+// public StudentProfile updateRepeatOffenderStatus(Long studentId) {
+// StudentProfile student = getStudentById(studentId);
+// student.setIsRepeatOffender(true);
+// return studentRepository.save(student);
+// }
+// }
 
 
 
