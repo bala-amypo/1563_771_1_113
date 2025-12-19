cb65c11412e7dbe9b57e33e0a3601ba32e48f3bd
bala-amypo <bala@amypo.in>
2025-12-19 07:44:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentProfile.java
+++ src/main/java/com/example/demo/entity/StudentProfile.java
@@ -53,21 +53,63 @@ public class StudentProfile {
     }
 
     
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+   public StudentProfile(){
 
-    public String getStudentId() { return studentId; }
-    public void setStudentId(String studentId) { this.studentId = studentId; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public Boolean getIsRepeatOffender() { return isRepeatOffender; }
-    public void setIsRepeatOffender(Boolean isRepeatOffender) { this.isRepeatOffender = isRepeatOffender; }
-
-    public List<IntegrityCase> getIntegrityCases() { return integrityCases; }
-    public void setIntegrityCases(List<IntegrityCase> integrityCases) { this.integrityCases = integrityCases; }
 }
+
+public Long getId() {
+return id;
+}
+public void setId(Long id) {
+this.id = id;
+}
+public String getStudentid() {
+return studentid;
+}
+public void setStudentid(String studentid) {
+this.studentid = studentid;
+}
+public String getName() {
+return name;
+}
+public void setName(String name) {
+this.name = name;
+}
+public String getEmail() {
+return email;
+}
+public void setEmail(String email) {
+this.email = email;
+}
+public String getProgram() {
+return program;
+}
+public void setProgram(String program) {
+this.program = program;
+}
+public Integer getYearLevel() {
+return yearLevel;
+}
+public void setYearLevel(Integer yearLevel) {
+this.yearLevel = yearLevel;
+}
+public Boolean getIsRepeatOffender() {
+return isRepeatOffender;
+}
+public void setIsRepeatOffender(Boolean isRepeatOffender) {
+this.isRepeatOffender = isRepeatOffender;
+}
+public LocalDateTime getCreatedAt() {
+return createdAt;
+}
+public void setCreatedAt(LocalDateTime createdAt) {
+this.createdAt = createdAt;
+}
+
+
+}
+
+
+
+
+
