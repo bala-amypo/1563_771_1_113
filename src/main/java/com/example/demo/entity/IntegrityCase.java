26efb6e65481efbec0f76786a8feee28f4a79275
bala-amypo <bala@amypo.in>
2025-12-19 04:53:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/IntegrityCase.java
+++ src/main/java/com/example/demo/entity/IntegrityCase.java
@@ -2,7 +2,25 @@ package com.example.demo.entity;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
+BUILD FAILURE
+[INFO] ------------------------------------------------------------------------
+[INFO] Total time:  21.977 s
+[INFO] Finished at: 2025-12-19T04:49:29Z
+[INFO] ------------------------------------------------------------------------
+[ERROR] Failed to execute goal org.apache.maven.plugins:maven-compiler-plugin:3.13.0:compile (default-compile) on project demo: Compilation failure
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/controller/IntegrityCaseController.java:[15,31] cannot access com.example.demo.entity.StudentProfile
+[ERROR]   bad source file: /home/coder/Workspace/demo/src/main/java/com/example/demo/entity/StudentProfile.java
+[ERROR]     file does not contain class com.example.demo.entity.StudentProfile
+[ERROR]     Please remove or make sure it appears in the correct subdirectory of the sourcepath.
+[ERROR] 
+[ERROR] -> [Help 1]
+[ERROR] 
+[ERROR] To see the full stack trace of the errors, re-run Maven with the -e switch.
+[ERROR] Re-run Maven using the -X switch to enable full debug logging.
+[ERROR] 
+[ERROR] For more information about the errors and possible solutions, please read the following articles:
+[ERROR] [Help 1] http://cwiki.apache.org/confluence/display/MAVEN/MojoFailureException
+coder@3e81dcb330ee:~/Workspace/demo$ 
 import jakarta.persistence.*;
 
 @Entity
