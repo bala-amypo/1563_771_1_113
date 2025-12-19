4cd41bf2a1bd36e398537472fc6e232bf74addf2
bala-amypo <bala@amypo.in>
2025-12-19 10:09:09 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentProfileServiceImpl.java
@@ -8,32 +8,35 @@ import org.springframework.stereotype.Service;
 import com.example.demo.entity.StudentProfile;
 import com.example.demo.repository.StudentProfileRepository;
 import com.example.demo.service.StudentProfileService;
-
 @Service
 public class StudentProfileServiceImpl implements StudentProfileService {
-
-    @Autowired
-     StudentProfileRepository studentRepository;
-
-    @Override
-    public StudentProfile createStudent(StudentProfile dto) {
-        return studentRepository.save(dto);
-    }
-
-    @Override
-    public StudentProfile getStudentById(Long id) {
-        return studentRepository.findById(id);
-    }
-
-    @Override
-    public List<StudentProfile> getAllStudents() {
-        return studentRepository.findAll();
-    }
-
-    @Override
-    public StudentProfile updateRepeatOffenderStatus(Long studentId) {
-        StudentProfile student = getStudentById(studentId);
-        student.setIsRepeatOffender(true);
-        return studentRepository.save(student);
-    }
+@Autowired
+private StudentProfileRepository studentRepository;
+@Override
+public StudentProfile createStudent(StudentProfile dto) {
+return studentRepository.save(dto);
 }
+@Override
+public StudentProfile getStudentById(Long id) {
+return studentRepository.findById(id)
+.orElseThrow(() -> new RuntimeException("Student not found"));
+}
+
+@Override
+public List<StudentProfile> getAllStudents() {
+return studentRepository.findAll();
+}
+
+@Override
+public StudentProfile updateRepeatOffenderStatus(Long studentId) {
+StudentProfile student = getStudentById(studentId);
+student.setIsRepeatOffender(true);
+return studentRepository.save(student);
+}
+}
+
+
+
+
+
+
