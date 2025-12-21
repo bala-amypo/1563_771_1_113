056a5b8e0d3e603f7b6d944b329b67fa11e7c55b
bala-amypo <bala@amypo.in>
2025-12-21 15:12:01 +0000

    User commit

--- src/main/java/com/example/demo/entity/RepeatOffenderRecord.java
+++ src/main/java/com/example/demo/entity/RepeatOffenderRecord.java
@@ -17,26 +17,45 @@ public class RepeatOffenderRecord {
     private LocalDate lastIncidentDate;
     private String flagSeverity;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public RepeatOffenderRecord(){
 
-    public StudentProfile getStudentProfile() { return studentProfile; }
+    }
+    public RepeatOffenderRecord(StudentProfile studentProfile, Integer totalCases, LocalDate lastincidentDate,
+            String flagSeverity) {
+        this.studentProfile = studentProfile;
+        this.totalCases = totalCases;
+        this.lastincidentDate = lastincidentDate;
+        this.flagSeverity = flagSeverity;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public StudentProfile getStudentProfile() {
+        return studentProfile;
+    }
     public void setStudentProfile(StudentProfile studentProfile) {
         this.studentProfile = studentProfile;
     }
-
-    public Integer getTotalCases() { return totalCases; }
+    public Integer getTotalCases() {
+        return totalCases;
+    }
     public void setTotalCases(Integer totalCases) {
         this.totalCases = totalCases;
     }
-
-    public LocalDate getLastIncidentDate() { return lastIncidentDate; }
-    public void setLastIncidentDate(LocalDate lastIncidentDate) {
-        this.lastIncidentDate = lastIncidentDate;
+    public LocalDate getLastincidentDate() {
+        return lastincidentDate;
     }
-
-    public String getFlagSeverity() { return flagSeverity; }
+    public void setLastincidentDate(LocalDate lastincidentDate) {
+        this.lastincidentDate = lastincidentDate;
+    }
+    public String getFlagSeverity() {
+        return flagSeverity;
+    }
     public void setFlagSeverity(String flagSeverity) {
         this.flagSeverity = flagSeverity;
     }
+   
 }
