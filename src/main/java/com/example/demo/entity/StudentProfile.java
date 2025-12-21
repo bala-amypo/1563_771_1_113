08a448e1e12f5343e5e6f25b4d64040c6bda6c67
bala-amypo <bala@amypo.in>
2025-12-21 13:26:57 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentProfile.java
+++ src/main/java/com/example/demo/entity/StudentProfile.java
@@ -23,15 +23,22 @@ public class StudentProfile {
     @Column(nullable = false)
     private boolean repeatOffender = false;
     private LocalDateTime createdAt;
-
+public StudentProfile(String StudentIdentifier, String name, String email, String program, Integer yearLevel,
+            Boolean isRepeatOffender, LocalDateTime createdAt) {
+        this.StudentIdentifier = StudentIdentifier;
+        this.name = name;
+        this.email = email;
+        this.program = program;
+        this.yearLevel = yearLevel;
+        this.isRepeatOffender = isRepeatOffender;
+        this.createdAt = createdAt;
+    }
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getStudentIdentifier() {
         return studentIdentifier;
     }
@@ -43,32 +50,38 @@ public class StudentProfile {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getEmail() {
         return email;
     }
-    
     public void setEmail(String email) {
         this.email = email;
     }
+    public String getProgram() {
+        return program;
+    }
+    public void setProgram(String program) {
+        this.program = program;
+    }
+    public Integer getYearLevel() {
+        return yearLevel;
+    }
+    public void setYearLevel(Integer yearLevel) {
+        this.yearLevel = yearLevel;
+    }
+    public Boolean getIsRepeatOffender() {
+        return isRepeatOffender;
+    }
+    public void setIsRepeatOffender(Boolean isRepeatOffender) {
+        this.isRepeatOffender = isRepeatOffender;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 
-    public String getDepartment() {
-        return department;
-    }
-    
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public boolean isRepeatOffender() {
-        return repeatOffender;
-    }
-
-    public void setRepeatOffender(boolean repeatOffender) {
-        this.repeatOffender = repeatOffender;
-    }
 }
