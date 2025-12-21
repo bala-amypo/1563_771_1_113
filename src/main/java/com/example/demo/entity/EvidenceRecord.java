6197df805e593da82608d4f7cd4f844d5094daed
bala-amypo <bala@amypo.in>
2025-12-21 15:02:56 +0000

    User commit

--- src/main/java/com/example/demo/entity/EvidenceRecord.java
+++ src/main/java/com/example/demo/entity/EvidenceRecord.java
@@ -23,4 +23,48 @@ public class EvidenceRecord {
         submittedAt = LocalDateTime.now();
     }
 
-    
+    public EvidenceRecord(IntegrityCase integrityCase, String evidenceType, String content, String submittedBy,
+            LocalDateTime submittedAt) {
+        this.integrityCase = integrityCase;
+        this.evidenceType = evidenceType;
+        this.content = content;
+        this.submittedBy = submittedBy;
+        this.submittedAt = submittedAt;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public IntegrityCase getIntegrityCase() {
+        return integrityCase;
+    }
+    public void setIntegrityCase(IntegrityCase integrityCase) {
+        this.integrityCase = integrityCase;
+    }
+    public String getEvidenceType() {
+        return evidenceType;
+    }
+    public void setEvidenceType(String evidenceType) {
+        this.evidenceType = evidenceType;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public String getSubmittedBy() {
+        return submittedBy;
+    }
+    public void setSubmittedBy(String submittedBy) {
+        this.submittedBy = submittedBy;
+    }
+    public LocalDateTime getSubmittedAt() {
+        return submittedAt;
+    }
+    public void setSubmittedAt(LocalDateTime submittedAt) {
+        this.submittedAt = submittedAt;
+    }
+   }
