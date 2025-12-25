a23ce626669e5554113e6120ea7ee469e347af27
bala-amypo <bala@amypo.in>
2025-12-25 16:04:27 +0000

    User commit

--- src/main/java/com/example/demo/dto/LoginRequest.java
+++ src/main/java/com/example/demo/dto/LoginRequest.java
@@ -1,6 +1,12 @@
 package com.example.demo.dto;
 
 public class LoginRequest {
-    public String username;
-    public String password;
+    private String email;
+    private String password;
+
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
 }
--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -1,6 +1,18 @@
 package com.example.demo.dto;
 
 public class RegisterRequest {
-    public String username;
-    public String password;
+    private String email;
+    private String password;
+    private String fullName;
+    private String role;
+
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+    public void setRole(String role) { this.role = role; }
+
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+    public String getFullName() { return fullName; }
+    public String getRole() { return role; }
 }
