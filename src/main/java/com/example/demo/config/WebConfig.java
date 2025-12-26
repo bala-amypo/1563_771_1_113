5d498c5b42df7645c0ef14e60d7139c26706a9bd
bala-amypo <bala@amypo.in>
2025-12-26 05:58:43 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+// CORS for your deployed frontend
+@Override
+public void addCorsMappings(CorsRegistry registry) {
+registry.addMapping("/api/**")
+.allowedOrigins("https://9230.pro604cr.amypo.ai/")
+.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+.allowedHeaders("*")
+.allowCredentials(true);
+}
+
+@Override
+public void addViewControllers(ViewControllerRegistry registry) {
+registry.addRedirectViewController("/", "/swagger-ui/index.html");
+}
+}
