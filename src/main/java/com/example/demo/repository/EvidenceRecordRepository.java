3ef9a5a0cc7671a01cd05b29d000bc5058ad627f
bala-amypo <bala@amypo.in>
2025-12-26 15:23:46 +0000

    User commit

--- src/main/java/com/example/demo/repository/EvidenceRecordRepository.java
+++ src/main/java/com/example/demo/repository/EvidenceRecordRepository.java
@@ -1,8 +1,12 @@
 package com.example.demo.repository;
+
 import com.example.demo.entity.EvidenceRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EvidenceRecordRepository extends JpaRepository<EvidenceRecord, Long> {
-    List<EvidenceRecord> findByIntegrityCase_Id(Long caseId);
+import java.util.List;   // ✅ FIX
 
- }
+public interface EvidenceRecordRepository
+        extends JpaRepository<EvidenceRecord, Long> {
+
+    List<EvidenceRecord> findByIntegrityCase_Id(Long caseId);
+}
