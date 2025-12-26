0fdba990d1da78c045573df54f40720cdd95a1ff
bala-amypo <bala@amypo.in>
2025-12-26 08:03:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/ErrorResponse.java
@@ -0,0 +1,41 @@
+package com.example.demo.dto;
+
+public class ErrorResponse {
+
+private int status;
+private String error;
+private String message;
+private String path;
+
+public int getStatus() {
+return status;
+}
+
+public void setStatus(int status) {
+this.status = status;
+}
+
+public String getError() {
+return error;
+}
+
+public void setError(String error) {
+this.error = error;
+}
+
+public String getMessage() {
+return message;
+}
+
+public void setMessage(String message) {
+this.message = message;
+}
+
+public String getPath() {
+return path;
+}
+
+public void setPath(String path) {
+this.path = path;
+}
+}
