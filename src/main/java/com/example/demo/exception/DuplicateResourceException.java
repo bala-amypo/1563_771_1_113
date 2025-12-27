043bbb799cc83f2dc70a08a5cec1d7bd62a68648
bala-amypo <bala@amypo.in>
2025-12-27 16:04:27 +0000

    User commit

--- src/main/java/com/example/demo/exception/DuplicateResourceException.java
+++ src/main/java/com/example/demo/exception/DuplicateResourceException.java
@@ -1,18 +1,16 @@
-// duplicate
+package com.example.demo.exception;
 
-// package com.example.demo.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.web.bind.annotation.ResponseStatus;
-
-// @ResponseStatus(HttpStatus.CONFLICT)
-// public class DuplicateResourceException extends RuntimeException {
-
-// public DuplicateResourceException(String message) {
-// super(message);
-// }
-
-// public DuplicateResourceException(String resourceName, String fieldName, Object fieldValue) {
-// super(String.format("%s already exists with %s : '%s'", resourceName, fieldName, fieldValue));
-// }
-// }
+@ResponseStatus(HttpStatus.CONFLICT)
+public class DuplicateResourceException extends RuntimeException {
+    
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+    
+    public DuplicateResourceException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s already exists with %s : '%s'", resourceName, fieldName, fieldValue));
+    }
+}
