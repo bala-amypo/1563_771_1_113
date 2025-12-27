cb39cf557309eb3c3cd21db09545faf09ba66dc3
bala-amypo <bala@amypo.in>
2025-12-27 10:07:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/PenaltyActionController.java
+++ src/main/java/com/example/demo/controller/PenaltyActionController.java
@@ -23,4 +23,28 @@ public class PenaltyActionController {
     public ResponseEntity<PenaltyAction> addPenalty(@RequestBody PenaltyAction p) {
         return ResponseEntity.ok(service.addPenalty(p));
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<PenaltyAction> getPenaltyActionById(
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(
+                penaltyActionService.getPenaltyActionById(id)
+        );
+    }
+
+    @GetMapping("/case/{caseId}")
+    public ResponseEntity<List<PenaltyAction>> getPenaltyActionsByCaseId(
+            @PathVariable Long caseId) {
+
+        return ResponseEntity.ok(
+                penaltyActionService.getPenaltyActionsByCaseId(caseId)
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PenaltyAction>> getAllPenaltyActions() {
+        return ResponseEntity.ok(
+                penaltyActionService.getAllPenaltyActions()
+        );
+    }
 }
