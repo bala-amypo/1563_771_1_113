88e7716b87478d8e829594935ee3ec5398b4c6ae
bala-amypo <bala@amypo.in>
2025-12-28 18:39:08 +0000

    User commit

--- src/main/java/com/example/demo/controller/PenaltyActionController.java
+++ src/main/java/com/example/demo/controller/PenaltyActionController.java
@@ -1,29 +1,29 @@
-package com.example.demo.controller;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import com.example.demo.entity.PenaltyAction;
-import com.example.demo.service.PenaltyActionService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+// package com.example.demo.controller;
+// import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+// import com.example.demo.entity.PenaltyAction;
+// import com.example.demo.service.PenaltyActionService;
+// import io.swagger.v3.oas.annotations.Operation;
+// import io.swagger.v3.oas.annotations.tags.Tag;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/penalties")
-@Tag(name = "Penalty Actions")
-@SecurityRequirement(name = "bearerAuth")
-public class PenaltyActionController {
-    private final PenaltyActionService service;
+// @RestController
+// @RequestMapping("/api/penalties")
+// @Tag(name = "Penalty Actions")
+// @SecurityRequirement(name = "bearerAuth")
+// public class PenaltyActionController {
+//     private final PenaltyActionService service;
 
-    public PenaltyActionController(PenaltyActionService service) {
-        this.service = service;
-    }
+//     public PenaltyActionController(PenaltyActionService service) {
+//         this.service = service;
+//     }
 
-    @PostMapping
-    @Operation(summary = "Add penalty")
-    public ResponseEntity<PenaltyAction> addPenalty(@RequestBody PenaltyAction p) {
-        return ResponseEntity.ok(service.addPenalty(p));
-    }
-}
+//     @PostMapping
+//     @Operation(summary = "Add penalty")
+//     public ResponseEntity<PenaltyAction> addPenalty(@RequestBody PenaltyAction p) {
+//         return ResponseEntity.ok(service.addPenalty(p));
+//     }
+// }
 
 
 
