a36f06595f886ab21bc205c2cdfc699d895ac9db
bala-amypo <bala@amypo.in>
2025-12-28 07:51:56 +0000

    User commit

--- src/main/java/com/example/demo/dto/ErrorResponse.java
+++ src/main/java/com/example/demo/dto/ErrorResponse.java
@@ -1,42 +1,42 @@
-package com.example.demo.dto;
+// package com.example.demo.dto;
 
-public class ErrorResponse {
+// public class ErrorResponse {
 
-    private int status;
-    private String error;
-    private String message;
-    private String path;
+//     private int status;
+//     private String error;
+//     private String message;
+//     private String path;
 
-    public int getStatus() {
-        return status;
-    }
+//     public int getStatus() {
+//         return status;
+//     }
 
-    public void setStatus(int status) {
-        this.status = status;
-    }
+//     public void setStatus(int status) {
+//         this.status = status;
+//     }
 
-    public String getError() {
-        return error;
-    }
+//     public String getError() {
+//         return error;
+//     }
 
-    public void setError(String error) {
-        this.error = error;
-    }
+//     public void setError(String error) {
+//         this.error = error;
+//     }
 
-    public String getMessage() {
-        return message;
-    }
+//     public String getMessage() {
+//         return message;
+//     }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
+//     public void setMessage(String message) {
+//         this.message = message;
+//     }
 
-    public String getPath() {
-        return path;
-    }
+//     public String getPath() {
+//         return path;
+//     }
 
-    public void setPath(String path) {
-        this.path = path;
-    }
-}
+//     public void setPath(String path) {
+//         this.path = path;
+//     }
+// }
  
